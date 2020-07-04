ec6f2fb50ffd09cd3d94acaec487c729ce97c977
SonalSingh18 <sonal.singh.19993@gmail.com>
2020-07-04 22:19:20 +0530

    violet: Import VioletParts
    Features:
    • Selinux Toggle
    • Toggle Usb Fast Charge
    
    Change-Id: I2947aa62b78b1150abde3484b7d3af5d97267f80
    Co-authored-by: StyloGey <stylogey@gmail.com>
    Co-authored-by: Rizky Benggolo <travarilo@gmail.com>
    Co-authored-by: Genkzsz11 <genkzsz11@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Co-authored-by: ronaxdevil <pratabidya.007@gmail.com>
    Co-authored-by: SiddharthBharadwaj <45765901+SiddharthBharadwaj@users.noreply.github.com>
    Co-authored-by: Saurabh Charde <saurabhchardereal@gmail.com>
    
    Adapted from: https://github.com/PainKiller3/device_xiaomi_sdm845-common/commit/1510fbe2049ef83b53fe971a6441ee26c47c3208
    by: @athul05
    
    Signed-off-by: Logan <athuldineshan5@gmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -184,7 +184,8 @@ PRODUCT_PACKAGES += \
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    VioletParts
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
@@ -450,6 +451,11 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
+
+#VioletParts init
+PRODUCT_PACKAGES += \
+    init.violetparts.sh \
+    init.violetparts.rc
 
 # Wifi
 PRODUCT_PACKAGES += \
