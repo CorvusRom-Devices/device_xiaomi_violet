d618abfaead3d26f610a8fea02706f78f5daf502
karthik558 <karthik.lal558@gmail.com>
2021-01-05 14:59:13 +0000

    violet: VioletParts: kcal: Added KCAL back
    * Will fix its nodes this time..
    
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>
    
    This reverts commit d25ebf5

--- VioletParts/src/com/xiaomi/parts/DeviceSettings.java
+++ VioletParts/src/com/xiaomi/parts/DeviceSettings.java
@@ -89,6 +89,15 @@ public class DeviceSettings extends PreferenceFragment implements
 
         String device = FileUtils.getStringProp("ro.build.product", "unknown");
 
+        PreferenceCategory displayCategory = (PreferenceCategory) findPreference(CATEGORY_DISPLAY);
+
+        mKcal = findPreference(PREF_DEVICE_KCAL);
+
+        mKcal.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), KCalSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        });
 
         mClearSpeakerPref = (Preference) findPreference(PREF_CLEAR_SPEAKER);
         mClearSpeakerPref.setOnPreferenceClickListener(preference -> {
