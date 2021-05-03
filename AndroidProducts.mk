fd87305321d66c6e8adba07bb69b6c1b62057325
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2021-05-03 16:44:08 +0000

    Legion Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_violet.mk
+    $(LOCAL_DIR)/legion_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_violet-userdebug \
-    aosp_violet-user
+    legion_violet-userdebug \
+    legion_violet-user
