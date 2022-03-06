ab43671c194586309323e975fe91e2a30c0f644f
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-03-06 19:07:27 +0530

    BoardConfig.mk: Switch to Proton Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
 TARGET_KERNEL_CONFIG := vendor/violet-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
 BOARD_RAMDISK_OFFSET := 0x01000000
 
 # Platform
