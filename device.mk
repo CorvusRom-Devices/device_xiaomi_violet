b7f901871713f16812ab8fe179a3f88fac892356
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-03-20 09:47:54 +0530

    device.mk: Remove unwanted libgui library

--- device.mk
+++ device.mk
@@ -146,7 +146,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
-    libgui_vendor \
     libxml2 \
     CameraGo \
     vendor.qti.hardware.camera.device@1.0.vendor
