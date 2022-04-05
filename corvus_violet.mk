9a9c02762e0389336297165735a26b5c8aed97ab
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-04-05 19:10:09 +0530

    corvus_violet.mk: Adapt Maintainer prop to the New Implementation
    Jolly ah Irukka (a pun) which means You are on Joy in Tamil.

--- corvus_violet.mk
+++ corvus_violet.mk
@@ -27,5 +27,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Corvus Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.corvus.maintainer= Tamizh Arasan
+CORVUS_MAINTAINER=Jolly_ah_Irukka
