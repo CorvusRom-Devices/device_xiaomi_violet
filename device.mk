a2796e717b0d3f69bbbb7438b636bf67126f75cd
Ravishanker V.G <no.1opensourcelover@protonmail.com>
2022-07-10 13:38:43 +0530

    Revert "violet: Go away Megvii"
    This reverts commit 40d3f291ebe1f2ff3a2f0d21480bad2822a7271c.

--- device.mk
+++ device.mk
@@ -142,10 +142,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
 
-PRODUCT_PACKAGES += \
-    libMegviiFacepp-0.5.2 \
-    libmegface
-
 # Config Store
 PRODUCT_PACKAGES += \
     disable_configstore
